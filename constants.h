a855897ce4eb3876d7f443b26db6d3803d7dbfeb
DC1MAK <DC1MAK@web.de>
2019-03-22 09:22:57 +0100

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,3 +1,7 @@
-
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+//
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
 #define REF_3V3 0x01
 #define REF_5V  0x02
