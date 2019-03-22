c17b3f47f06ecf0a03205727953f45a84505087b
DC1MAK <DC1MAK@web.de>
2019-03-22 09:16:54 +0100

    Update AFSK.cpp

--- AFSK.cpp
+++ AFSK.cpp
@@ -1,4 +1,8 @@
-
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+//
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
 #include <string.h>
 #include "AFSK.h"
 #include "Arduino.h"
