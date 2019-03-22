5c6ad7bf471728327c835f19c25d6da851f494f6
DC1MAK <DC1MAK@web.de>
2019-03-22 09:19:01 +0100

    Update AX25.cpp

--- AX25.cpp
+++ AX25.cpp
@@ -1,5 +1,11 @@
 
 // Based on work by Francesco Sacchi
+
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
+
 #include "Arduino.h"
 #include <string.h>
 #include <ctype.h>
